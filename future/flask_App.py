53e37b144bc7eb4fe01afc6ef3880a81cfaeca65
Oskar Jansson <oskja067@student.liu.se>
2017-01-07 17:06:28 +0100

    Added backbone for cards

--- future/flask_App.py
+++ future/flask_App.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+from card import Card
+from data import Data
+import jinja2
 app = Flask(__name__)
-
+d = Data()
 @app.route('/')
 def index():
-    return 'Hello World'
+    d.load_JSON('data.json')
+    cards = d.get_cards()
+    return render_template('index.html', cards = cards)
 
 if __name__ == '__main__':
     app.run()
